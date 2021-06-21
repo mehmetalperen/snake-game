a2e3c57a75ceeee6efe0902d6ec93e5a18c6a2cb
Mehmet Nadi <mhmtalperennadi@gmail.com>
2021-06-20 20:58:42 -0700

    count score

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let lastRenderTime = 0;
-const SNAKE_SPEED = 2; //move snake by 2 boxes every second
+const SNAKE_SPEED = 3; //move snake by 2 boxes every second
 const gameBoard = document.getElementById("game-board");
 let snakeBody = [];
 let snakeDirection = {};
 let isGameOver;
 let foodPos = {};
+let score;
 
 function main(currentTime) {
   //game loop
@@ -19,7 +20,7 @@ function main(currentTime) {
   if (!isGameOver) {
     draw(gameBoard);
   } else {
-    alert("game is over my guy");
+    alert(`Game is over! Your score is ${score}`);
     initNewgame();
   }
 }
@@ -61,7 +62,6 @@ function isHitBorder() {
     snakeBody[0].y > 21
   ) {
     isGameOver = true;
-    console.log(`hit border: ${snakeBody[0].x} and ${snakeBody[0].y}`);
   }
 }
 
@@ -72,47 +72,39 @@ function isSnakeDead() {
       snakeBody[0].y === snakeBody[i].y
     ) {
       isGameOver = true;
-      console.log(`killed: ate ${i}`);
     }
   }
 }
 function isFoodEaten() {
   if (snakeBody[0].x === foodPos.x && snakeBody[0].y === foodPos.y) {
     setFoodPos();
+    snakeBody.push(snakeBody[snakeBody.length - 1]);
+    score++;
   }
 }
 
 function eventListener() {
-  console.log("sub");
   document.addEventListener("keydown", (event) => {
     if (event.keyCode === 38) {
-      console.log("up");
       if (snakeDirection.yDirection === 1) {
-        console.log("cant go back");
       } else {
         snakeDirection.yDirection = -1;
         snakeDirection.xDirection = 0;
       }
     } else if (event.keyCode === 40) {
-      console.log("down");
       if (snakeDirection.yDirection === -1) {
-        console.log("cant go back");
       } else {
         snakeDirection.yDirection = 1;
         snakeDirection.xDirection = 0;
       }
     } else if (event.keyCode === 39) {
-      console.log("right");
       if (snakeDirection.xDirection === -1) {
-        console.log("cant go back");
       } else {
         snakeDirection.yDirection = 0;
         snakeDirection.xDirection = 1;
       }
     } else if (event.keyCode === 37) {
-      console.log("left");
       if (snakeDirection.xDirection === 1) {
-        console.log("cant go back");
       } else {
         snakeDirection.yDirection = 0;
         snakeDirection.xDirection = -1;
@@ -125,8 +117,6 @@ function initNewgame() {
     { x: 4, y: 11 },
     { x: 3, y: 11 },
     { x: 2, y: 11 },
-    { x: 1, y: 11 },
-    { x: 0, y: 11 },
   ];
   snakeDirection = {
     xDirection: 1,
@@ -134,6 +124,7 @@ function initNewgame() {
   };
   isGameOver = false;
   setFoodPos();
+  score = 0;
 }
 
 eventListener();
